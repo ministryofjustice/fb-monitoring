38204a9323edafc706ba0d2b66e1fa46d3fb7078
Jesus Laiz <zheileman@users.noreply.github.com>
2023-12-07 11:47:00 +0000

    Using contexts

--- scripts/circleci_configure_context_eks.sh
+++ scripts/circleci_configure_context_eks.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-alert=$1
 platform_env=$2
 deployment_env=$3
 
-string="${alert}_${platform_env}_${deployment_env}"
+# test_dev, test_production, live_dev, live_production
+string="${platform_env}_${deployment_env}"
 string=$(echo $string | awk '{ gsub(/_$/, ""); print }')
 
 upcase_string=$(echo $string | tr a-z A-Z)
-token=$(eval echo "\$EKS_${upcase_string}_TOKEN" | base64 -d)
+token=$(eval echo "\$EKS_TOKEN_${upcase_string}" | base64 -d)
 
 echo "kubectl configure credentials"
 kubectl config set-credentials "circleci_${string}" --token="${token}"
 
 echo "kubectl configure context"
-kubectl config set-context "circleci_${string}" --cluster="$EKS_CLUSTER" --user="circleci_${string}"
+kubectl config set-context "circleci_${string}" --cluster="$EKS_CLUSTER_NAME" --user="circleci_${string}"
 
 echo "kubectl use circleci context"
 kubectl config use-context "circleci_${string}"
