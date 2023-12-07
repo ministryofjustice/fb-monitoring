b6236815628e94f40a5fa0926976e3038145c484
Jesus Laiz <zheileman@users.noreply.github.com>
2023-12-07 12:08:18 +0000

    Tweaks

--- scripts/circleci_configure_context_eks.sh
+++ scripts/circleci_configure_context_eks.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
+destination=$1
 platform_env=$2
 deployment_env=$3
 
 # test_dev, test_production, live_dev, live_production
 string="${platform_env}_${deployment_env}"
-string=$(echo $string | awk '{ gsub(/_$/, ""); print }')
+string=$(echo "$string" | awk '{ gsub(/_$/, ""); print }')
 
-upcase_string=$(echo $string | tr a-z A-Z)
-token=$(eval echo "\$EKS_TOKEN_${upcase_string}" | base64 -d)
+upcase_string=$(echo "$string" | tr a-z A-Z)
+token=$(eval echo "\$EKS_TOKEN_${upcase_string}")
 
-echo "kubectl configure credentials"
-kubectl config set-credentials "circleci_${string}" --token="${token}"
+user="circleci_${string}"
+namespace="formbuilder-${destination}-${platform_env}-${deployment_env}"
 
-echo "kubectl configure context"
-kubectl config set-context "circleci_${string}" --cluster="$EKS_CLUSTER_NAME" --user="circleci_${string}"
+echo "kubectl configure credentials for user $user"
+kubectl config set-credentials "${user}" --token="${token}"
 
-echo "kubectl use circleci context"
-kubectl config use-context "circleci_${string}"
+echo "kubectl configure context for namespace $namespace"
+kubectl config set-context "${user}" --cluster="$EKS_CLUSTER_NAME" --user="${user}" --namespace="${namespace}"
+
+echo "kubectl use context"
+kubectl config use-context "${user}"
